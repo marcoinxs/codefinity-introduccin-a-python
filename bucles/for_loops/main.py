51d03773ab1bb7f5a1c7ad18c6af4399fd07e0fe
marcoinxs <>
2026-04-07 22:27:04 +0000

    For Loops

---
+++ bucles/for_loops/main.py
@@ -0,0 +1,11 @@
+prices = [12.99, 8.50, 15.75, 23.00, 7.25]
+total = 0
+# Write your code here
+for item in prices:
+    # Code to be executed
+    print(item)
+    total = total + item
+
+print(total)
+
+
